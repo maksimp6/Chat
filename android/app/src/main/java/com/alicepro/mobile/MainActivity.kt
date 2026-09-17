51d8831deb7cbc4db844ec6aba91e074bcc88288
maksimp6 <maksimp88887@gmail.com>
2026-09-17 19:22:46 +0300

    fix(android): start bundled Python Flask server before WebView

--- android/app/src/main/java/com/alicepro/mobile/MainActivity.kt
+++ android/app/src/main/java/com/alicepro/mobile/MainActivity.kt
@@ -4,10 +4,23 @@ import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import com.chaquo.python.Python
+import com.chaquo.python.android.AndroidPlatform
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (!Python.isStarted()) {
+            Python.start(AndroidPlatform(this))
+        }
+
+        Thread {
+            Python.getInstance()
+                .getModule("android_server")
+                .callAttr("start_server")
+        }.start()
+
         val webView = WebView(this)
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
