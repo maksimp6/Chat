40c8cb4dbc02cd238c8951b5e9684a31ca91c29c
maksimp6 <maksimp88887@gmail.com>
2026-09-17 19:20:13 +0300

    feat(android): add WebView shell for existing Flask UI

---
+++ android/app/src/main/java/com/alicepro/mobile/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.alicepro.mobile
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("http://127.0.0.1:5000")
+        setContentView(webView)
+    }
+}
