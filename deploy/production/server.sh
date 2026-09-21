bd0cb83891893222542b97f0a2cc4fca0111e5e4
John Smith <maksimp88887@gmail.com>
2026-09-21 15:48:42 +0300

    Remove duplicate ACME router labels

--- deploy/production/server.sh
+++ deploy/production/server.sh
@@ -83,9 +83,6 @@ deploy() {
     --label "traefik.http.routers.alice-production-https.tls.certresolver=letsencrypt" \
     --label "traefik.http.routers.alice-production-https.tls.domains[0].main=maxxxpavlov.ru" \
     --label "traefik.http.routers.alice-production-https.tls.domains[0].sans[0]=maxxxpavlov.online" \
-    --label "traefik.http.routers.alice-production-https.tls.certresolver=letsencrypt" \
-    --label "traefik.http.routers.alice-production-https.tls.domains[0].main=maxxxpavlov.ru" \
-    --label "traefik.http.routers.alice-production-https.tls.domains[0].sans[0]=maxxxpavlov.online" \
     --label "traefik.http.routers.alice-production-https.service=alice-production" \
     --label "traefik.http.middlewares.alice-production-https-redirect.redirectscheme.scheme=https" \
     --label "traefik.http.middlewares.alice-production-https-redirect.redirectscheme.permanent=true" \
