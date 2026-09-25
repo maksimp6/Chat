49c14ba72e7f889ca13bd2f69f5f46dfab71d3d9
John Smith <maksimp88887@gmail.com>
2026-09-26 02:25:58 +0300

    test(issue-modal): use unified modal content class

--- static/file_manager.js
+++ static/file_manager.js
@@ -51,7 +51,7 @@ window.fetchVectorStores = function() {
         ov.className = "modal file-manager-modal";
 
         var md = document.createElement("div");
-        md.className = "file-manager-box";
+        md.className = "modal-content file-manager-box";
 
         md.innerHTML = [
             '<div style="display:flex;justify-content:space-between;align-items:center;margin-bottom:16px;">',
